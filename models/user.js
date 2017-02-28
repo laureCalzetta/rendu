adeeda278f7da9816833cd0c877425298efe549e
Laure Calzetta <laure.calzetta@gmail.com>
2017-02-28 14:52:28 +0100

    Modified User

--- models/user.js
+++ models/user.js
@@ -1,27 +1,68 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// Define the schema for users
+
+
+/**
+ * A user with a firstname, a lastname, a roles et the date. 
+ * Two users must not have the same first and last name
+ */
+
 const userSchema = new Schema({
+
+  // firstName - String, 2-20 characters
   firstname: {
-  	type: String, minlength: 2, maxlength: 20, index: true
+    type: String,
+    minlength: 2,
+    maxlength: 20,
+    required: true
   },
+
+  // lastName - String, 2-20 characters
   lastname: {
-  	type: String, minlength: 2, maxlength: 20, index: true
+    type: String,
+    minlength: 2,
+    maxlength: 20,
+    required: true
   },
-  // set date by default at now, anyway we override it below with .virtual
-  date: {
-  	type: Date, default: Date.now
+  // createdAt - Date, the date at which the user was registered
+  //set date by default at now, anyway we override it below with .virtual
+  createdAte: {
+    type: Date,
+    default: Date.now
   },
+
+  // role - String, "citizen" or "manager"
   roles: {
-  	type: String, enum: ['citizen', 'manager']
+    type: String,
+    enum: ['citizen', 'manager'],
+    required: true
   }
 });
 
-// force value to be the current date
-//userSchema.virtual('date').get(function ()
-//{
-//    return Date.now;
-//});
+// Customize the behavior of user.toJSON() (called when using res.send)
+userSchema.set('toJSON', {
+  transform: transformJsonUser, // Modify the serialized JSON with a custom function
+  virtuals: true // Include virtual properties when serializing documents to JSON
+});
+
+
+/**
+ * Given a firstname and lastname, calls the callback function with true if no user exists with that firstname and lastname
+ */
+userSchema.index({ firstname: 1, lastname: 1  }, { unique: true });
+
+/**
+ * Removes extra MongoDB properties from serialized people.
+ */
+function transformJsonUser(doc, json, options) {
+
+  // Remove MongoDB _id & __v (there's a default virtual "id" property)
+  delete json._id;
+  delete json.__v;
+
+  return json;
+}
+
 
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 const User = require('../models/user');
+const Issue = require('../models/issue');
 
-/* GET users listing. */
+
+/* GET users listing */
 router.get('/', function(req, res, next) {
-    User.find().sort('name').exec(function(err, users) {
+  User.find().sort('name').exec(function(err, users) {
     if (err) {
       return next(err);
     }
@@ -13,18 +14,78 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET find a user */
+router.get('/:user_id', function(req, res, next) {
+
+  User.findById(req.params.user_id, function(err, user) {
+         if (err) {
+        return next(err);
+      }
+      res.send(user);
+    });
+});
+
+
 /* POST new user */
 router.post('/', function(req, res, next) {
   // Create a new document from the JSON in the request body
-  const newUser = new User(req.body);
+  var newUser = new User(req.body);
   // Save that document
   newUser.save(function(err, savedUser) {
     if (err) {
-      return next(err);
+      res.status(500).send(err);
+      return;
     }
     // Send the saved document in the response
     res.send(savedUser);
   });
 });
+
+router.put('/:user_id', function(req, res, next) {
+   User.findById(req.params.user_id, function(err, user) {
+    if (err) {
+      return next(err);
+    }
+
+  user.firstname = req.body.firstname;
+  user.lastname = req.body.lastname;
+  user.roles = req.body.roles;
+
+    user.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+    });
+
+    res.send(user);
+  });
+
+});
+
+router.patch('/:user_id', function(req, res, next) {
+   User.findById(req.params.user_id, function(err, user) {
+
+    if (req.body.firstname !== undefined) {
+      user.firstname = req.body.firstname;
+    }
+    if (req.body.lastname !== undefined) {
+      user.lastname = req.body.lastname;
+    }
+    if (req.body.roles !== undefined) {
+      user.roles = req.body.roles;
+    }
+
+    user.save(function(err) {
+        if (err) {
+        return next(err);
+      }
+
+    });
+
+    res.send(user);
+  });
+
+});
+
 
 module.exports = router;
