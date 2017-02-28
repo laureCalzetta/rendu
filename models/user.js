44ecc29d6d71301e63380077cbcf0db88745898f
Laure Calzetta <laure.calzetta@gmail.com>
2017-02-28 10:11:53 +0100

    version qui tourne

--- models/user.js
+++ models/user.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-  name: String
+  firstname: { type: String, minlength: 2, maxlength: 20, index: true },
+  lastname: { type: String, minlength: 2, maxlength: 20, index: true },
+  // set date by default at now, anyway we override it below with .virtual
+  date: { type: Date, default: Date.now },
+  roles: { type: String, enum: ['citizen', 'manager']}
 });
+
+// force value to be the current date
+userSchema.virtual('date').get(function () {
+    return Date.now;
+});
+
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
