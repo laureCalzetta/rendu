c659befdaad1a695fcb6f915af7202f5ce7f1b67
Laure Calzetta <laure.calzetta@gmail.com>
2017-02-28 15:44:09 +0100

    Mise à jour

--- app.js
+++ app.js
@@ -5,33 +5,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my-db-name');
-
-
-var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.use(function myMiddleware(req, res, next) {
-  console.log('Hello World!');
-  next();
-});
 
-// view engine setup
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my-db-name');
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// General middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
-app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
